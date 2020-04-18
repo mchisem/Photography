9d6b9d9c515642172053ee7162ead90d7a5f423e
Maya Chisem <chisem.maya@gmail.com>
2020-04-18 14:22:24 -0700

    change

--- js/picture.js
+++ js/picture.js
@@ -3,6 +3,7 @@ var modal = document.getElementById("back");
 var span = document.querySelector("#close");
 var images = document.querySelector(".image");
 var modalImg = document.querySelector(".modal-content");
+var content = document.querySelector("#content");
 
 // Event delegation for images
 const parent = document.querySelector(".container");
@@ -14,6 +15,7 @@ parent.addEventListener("click", event => {
   //if you console.log(this) -> returns the whole window object which has no src value
   //using event.target can make us target exactly what we need
   modalImg.src = event.target.src;
+  content.classList.remove("hide");
   span.classList.remove("hide");
   }
   })
@@ -21,4 +23,5 @@ parent.addEventListener("click", event => {
 // When the user clicks on (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  content.classList.add("hide");
 }
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,15 @@
-// variables //
-var window = $(window);
-var element = $(el);
+new fullpage ("#fullpage", {
+    autoScrolling: true,
+    navigation: true,
+    onLeave: (origin, destination, directon) => {
 
-var image = $("img");
+        const tl = new TimelineMax({delay:05});
 
-window.scroll(function(event){
-
+        if(destination.index === 1) {
+            const image = document.querySelectorAll(".img");
+    
+            tl.fromTo(image, 0.5, {x: "0%"}, {x: "40%"});
+        }
+    }
+   
 })
