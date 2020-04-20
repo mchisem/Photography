9c4937f40a45db7868a5d2f4d48ccfa8482df8b7
Maya Chisem <chisem.maya@gmail.com>
2020-04-20 10:20:45 -0700

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,2 @@
-new fullpage ("#fullpage", {
-    autoScrolling: true,
-    navigation: true,
-    onLeave: (origin, destination, directon) => {
 
-        const tl = new TimelineMax({delay:05});
 
-        if(destination.index === 1) {
-            const image = document.querySelectorAll(".img");
-    
-            tl.fromTo(image, 0.5, {x: "0%"}, {x: "40%"});
-        }
-    }
-   
-})
